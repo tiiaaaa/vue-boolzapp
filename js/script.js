8f99fdc17ab82f558694a03dea46f17592ec9003
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-22 18:49:04 +0100

    modificati oggetti per dare immagini ai contatti e aggiunto cursor pointer sui vari contatti

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const app = new Vue({
         contacts: [
             {
                 name: 'Michele',
-                avatar: '_1',
+                avatar: 'img/avatar_1.jpg',
                 visible: true,
                 messages: [
                     {
@@ -26,7 +26,7 @@ const app = new Vue({
             },
             {
                 name: 'Fabio',
-                avatar: '_2',
+                avatar: 'img/avatar_2.jpg',
                 visible: true,
                 messages: [
                     {
@@ -48,7 +48,7 @@ const app = new Vue({
             },
             {
                 name: 'Samuele',
-                avatar: '_3',
+                avatar: 'img/avatar_3.jpg',
                 visible: true,
                 messages: [
                     {
@@ -70,7 +70,7 @@ const app = new Vue({
             },
             {
                 name: 'Alessandro B.',
-                avatar: '_4',
+                avatar: 'img/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
@@ -87,7 +87,7 @@ const app = new Vue({
             },
             {
                 name: 'Alessandro L.',
-                avatar: '_5',
+                avatar: 'img/avatar_5.jpg',
                 visible: true,
                 messages: [
                     {
@@ -104,7 +104,7 @@ const app = new Vue({
             },
             {
                 name: 'Claudia',
-                avatar: '_6',
+                avatar: 'img/avatar_6.jpg',
                 visible: true,
                 messages: [
                     {
@@ -126,7 +126,7 @@ const app = new Vue({
             },
             {
                 name: 'Federico',
-                avatar: '_7',
+                avatar: 'img/avatar_7.jpg',
                 visible: true,
                 messages: [
                     {
@@ -143,7 +143,7 @@ const app = new Vue({
             },
             {
                 name: 'Davide',
-                avatar: '_8',
+                avatar: 'img/avatar_8.jpg',
                 visible: true,
                 messages: [
                     {
@@ -164,6 +164,8 @@ const app = new Vue({
                 ],
             }
         ]
+
+        
     }
     
 })
